c0c9cfe71e3ad5955ec2b8dc86e022dec76b3722
Suraj Kumar Mishra <101392142+surajkm24@users.noreply.github.com>
2022-12-23 02:50:45 +0000

    separated styles in another file

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname + '/views/'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
